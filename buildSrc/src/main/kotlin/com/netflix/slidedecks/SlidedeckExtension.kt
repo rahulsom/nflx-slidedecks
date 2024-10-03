f1a2eb7062ab70f7bd76833fa2d53dce1cbeefd4
Rahul Somasunderam <rahulsom@noreply.github.com>
2024-10-03 13:16:44 +0530

    chore: Fix formatting

--- buildSrc/src/main/kotlin/com/netflix/slidedecks/SlidedeckExtension.kt
+++ buildSrc/src/main/kotlin/com/netflix/slidedecks/SlidedeckExtension.kt
@@ -6,19 +6,20 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import java.time.LocalDate
 
 open class SlidedeckExtension(project: Project) {
-    fun toJson(): String {
-        return ObjectMapper().writeValueAsString(mapOf(
-            "date" to date.getOrNull().toString(),
-            "title" to title.getOrNull().toString(),
-            "venue" to venue.getOrNull().toString(),
-            "video" to video.getOrNull().toString()
-        ))
-    }
+  fun toJson(): String {
+    return ObjectMapper().writeValueAsString(
+      mapOf(
+        "date" to date.getOrNull().toString(),
+        "title" to title.getOrNull().toString(),
+        "venue" to venue.getOrNull().toString(),
+        "video" to video.getOrNull().toString()
+      )
+    )
+  }
 
-    val date = project.objects.property<LocalDate>(LocalDate::class.java)
-    val title: Property<String> = project.objects.property<String>(String::class.java)
-    val venue: Property<String> = project.objects.property<String>(String::class.java)
-    val video: Property<String> = project.objects.property<String>(String::class.java)
-    val theme: Property<String> = project.objects.property<String>(String::class.java)
-
+  val date = project.objects.property<LocalDate>(LocalDate::class.java)
+  val title: Property<String> = project.objects.property<String>(String::class.java)
+  val venue: Property<String> = project.objects.property<String>(String::class.java)
+  val video: Property<String> = project.objects.property<String>(String::class.java)
+  val theme: Property<String> = project.objects.property<String>(String::class.java)
 }
--- buildSrc/src/main/kotlin/com/netflix/slidedecks/SlidedeckPlugin.kt
+++ buildSrc/src/main/kotlin/com/netflix/slidedecks/SlidedeckPlugin.kt
@@ -12,7 +12,7 @@ import org.gradle.api.Project
 import org.gradle.api.plugins.ExtensionAware
 
 class SlidedeckPlugin : Plugin<Project> {
-  override fun apply( project:Project) {
+  override fun apply(project: Project) {
     val extension = project.extensions.create("slidedeck", SlidedeckExtension::class.java, project)
     if (!extension.theme.isPresent) {
       extension.theme.set("netflix")
@@ -23,30 +23,30 @@ class SlidedeckPlugin : Plugin<Project> {
     project.repositories.mavenCentral()
     val repositoriesInternal = project.repositories as ExtensionAware
     repositoriesInternal.extensions.configure(com.github.jrubygradle.api.core.RepositoryHandlerExtension::class.java) {
-        it.gems()
+      it.gems()
     }
     project.extensions.findByType<RevealJSExtension>(RevealJSExtension::class.java)!!.apply {
       version = "3.1.0"
       templateGitHub(Action {
-        it.setOrganisation ("hakimel")
-        it.setRepository( "reveal.js")
-        it.setTag ("3.9.1")
+        it.setOrganisation("hakimel")
+        it.setRepository("reveal.js")
+        it.setTag("3.9.1")
       })
     }
     project.extensions.findByType(AsciidoctorJExtension::class.java)!!.apply {
       modules {
         it.diagram.use()
       }
-      attributes (mapOf("includedir" to "${project.projectDir}/src/docs/asciidoc"))
+      attributes(mapOf("includedir" to "${project.projectDir}/src/docs/asciidoc"))
     }
     project.tasks.withType(AsciidoctorJRevealJSTask::class.java).configureEach {
       it.revealjsOptions { o ->
-        o.setControls (false)
-        o.setOverviewMode( true)
-        o.setProgressBar (true)
-        o.setPushToHistory( true)
+        o.setControls(false)
+        o.setOverviewMode(true)
+        o.setProgressBar(true)
+        o.setPushToHistory(true)
         o.setCustomThemeLocation("build/sass/${extension.theme.get()}.css")
-        o.setVerticalCenter( false)
+        o.setVerticalCenter(false)
       }
     }
     project.tasks.withType(DeckTapeTask::class.java).configureEach {
@@ -61,16 +61,16 @@ class SlidedeckPlugin : Plugin<Project> {
       val buildDir = project.buildDir
       it.doLast {
         project.copy {
-          it.from ("${rootDir}/stylesheet/build/sass")
-          it.into ("build/sass")
+          it.from("${rootDir}/stylesheet/build/sass")
+          it.into("build/sass")
         }
         project.copy {
-          it.from ("${rootDir}/stylesheet/src/main/sass")
-          it.into ("${buildDir}/docs/asciidocRevealJs/style")
+          it.from("${rootDir}/stylesheet/src/main/sass")
+          it.into("${buildDir}/docs/asciidocRevealJs/style")
         }
         project.copy {
-          it.from ("${rootDir}/stylesheet/build/docs/asciidocRevealJs/package")
-          it.into ("${buildDir}/docs/asciidocRevealJs/package")
+          it.from("${rootDir}/stylesheet/build/docs/asciidocRevealJs/package")
+          it.into("${buildDir}/docs/asciidocRevealJs/package")
         }
       }
       it.inputs.dir("${rootDir}/stylesheet/build/sass")
@@ -81,32 +81,33 @@ class SlidedeckPlugin : Plugin<Project> {
     project.tasks.findByName("asciidoctorRevealJs")!!.apply {
       dependsOn("copyStyles")
       doLast {
-        println ("Open ${project.buildDir}/docs/asciidocRevealJs/index.html")
+        println("Open ${project.buildDir}/docs/asciidocRevealJs/index.html")
       }
     }
     project.tasks.create("copyToPages") {
       val rootDir = project.rootDir
       it.doLast {
         project.copy {
-          it.from ("build/docs/asciidocRevealJs")
-          it.into ("${rootDir}/pages/build/staging/${project.name}/html")
+          it.from("build/docs/asciidocRevealJs")
+          it.into("${rootDir}/pages/build/staging/${project.name}/html")
         }
         project.copy {
-          it.from ("build/docs/asciidocRevealJsExport")
-          it.into ("${rootDir}/pages/build/staging/${project.name}/pdf")
+          it.from("build/docs/asciidocRevealJsExport")
+          it.into("${rootDir}/pages/build/staging/${project.name}/pdf")
         }
         project.file("${rootDir}/pages/build/staging/${project.name}").mkdirs()
-        project.file("${rootDir}/pages/build/staging/${project.name}/metadata.json").writeText( extension.toJson())
+        project.file("${rootDir}/pages/build/staging/${project.name}/metadata.json")
+          .writeText(extension.toJson())
       }
       it.inputs.property("slidedeckExtension", extension.toJson())
-      it.inputs.dir ("build/docs/asciidocRevealJs")
-      it.inputs.dir ("build/docs/asciidocRevealJsExport")
+      it.inputs.dir("build/docs/asciidocRevealJs")
+      it.inputs.dir("build/docs/asciidocRevealJsExport")
 
       it.outputs.file("${rootDir}/pages/build/staging/${project.name}/metadata.json")
-      it.outputs.dir ("${rootDir}/pages/build/staging/${project.name}/html")
-      it.outputs.dir ("${rootDir}/pages/build/staging/${project.name}/pdf")
+      it.outputs.dir("${rootDir}/pages/build/staging/${project.name}/html")
+      it.outputs.dir("${rootDir}/pages/build/staging/${project.name}/pdf")
 
-      it.dependsOn ("asciidoctorRevealJsExport")
+      it.dependsOn("asciidoctorRevealJsExport")
     }
 
     project.tasks.getByName("build").dependsOn("asciidoctorRevealJsExport", "copyToPages")
